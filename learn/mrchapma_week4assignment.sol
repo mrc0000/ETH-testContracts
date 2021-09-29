1d437c1cedb38d1551c0458e5b7c15ffb1a1f8c9
"mrc0000" <"mchapman367@gmail.com">
2021-09-28 22:19:43 -0500

    added remove function for contract owner and global variable to keep track of total students

--- learn/mrchapma_week4assignment.sol
+++ learn/mrchapma_week4assignment.sol
@@ -23,6 +23,8 @@ contract MyStudentContract {
     //creating array of multiple students
     studentAccount [] public allStudents;
     
+   uint public totalStudents;
+    
     address owner;
     
     constructor() public {
@@ -30,19 +32,9 @@ contract MyStudentContract {
         }
         
     //modifier to make sure the caller owns the contract
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not Owner");
-        
-        //underscore is a special scharacter only used inside
-        //a function modifier and it tells solidity to
-        //execute the rest of the code
-        _;
-        
-    }
+    
     
     event AddedStudents (string studentName);
-    
-    
     //Create a function to add students - made it public so someone can click on a button and the user can pass on the information
     //anything inside the function only exists in the function and then goes away after running the transaction
     //anything OUTSIDE of the function is global and lives outside on the blockchain
@@ -59,13 +51,34 @@ contract MyStudentContract {
     theLevel:NewProgramLevel,
     BlockchainTrackEnrolled:NewBlockchainTrackEnrolled
     
-    
-    
-    
     //when you create a struct you HAVE to load everything at once
     });
     
-    //must save it to a global variables
+    //push newStudent instance data to the allStudents array, emit the newstudent name using AddedStudent Event then -
+    //set the total students public variable to the value of the allStudents array length
     allStudents.push(newStudent);
+    emit AddedStudents(newStudent.UserName);
+    totalStudents = allStudents.length;
     }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not Owner");
+        
+        //underscore is a special scharacter only used inside
+        //a function modifier and it tells solidity to
+        //execute the rest of the code
+        _;
+        
+    }
+    
+    function remove(uint index) public {
+        
+        // Move the last element into the place to delete
+        allStudents[index] = allStudents[allStudents.length - 1];
+        // Remove the last element
+        delete allStudents[index];
+        totalStudents = allStudents.length;
+
+    }
+    
 }
